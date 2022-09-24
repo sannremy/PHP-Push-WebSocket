aa1c61a27ed95db53d74515e6e1a14782a9501f4
Sann-Remy <667617+sannremy@users.noreply.github.com>
2022-09-24 15:00:52 +0200

    Update Client.php

--- src/PushWebSocket/Client.php
+++ src/PushWebSocket/Client.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Define a Client object
- * @author Sann-Remy Chea <http://srchea.com>
+ * @author Sann-Remy Chea <http://sannremy.com>
  * @license This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
